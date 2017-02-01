4a2cc71431ec992c30ede0524a09400e876cd895
Josiah Allen <josiah.allen@mainstreethub.com>
2017-02-01 13:31:48 -0600

    run tree walker on search and display results in console

--- content.js
+++ content.js
@@ -2,4 +2,20 @@ console.log('hello content');
 
 chrome.runtime.onMessage.addListener(function(msg, sender) {
 	console.log(msg, sender);
+	runWalker(msg);
 })
+
+function runWalker(searchWord) {
+	var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, function(node) {
+		var foundWord = matchWord(searchWord, node.textContent);
+		if(foundWord !== -1) {
+			console.log(node.textContent);
+		}
+		return NodeFilter.FILTER_SKIP;
+	}, false);
+	walker.nextNode();
+}
+
+function matchWord(searchWord, textNode) {
+	return textNode.indexOf(searchWord)
+}
